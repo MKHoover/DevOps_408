07fd3ccb6202d6336334b83d757b4a974b491c18
Maia_Hoover <maia@hoovershultz.com>
2023-11-04 23:14:03 -0700

    finished the script

--- Help_the_elves.sh
+++ Help_the_elves.sh
@@ -20,7 +20,7 @@ current_elf=0
 
 sum=0
 
-while read -r line
+while read -r line;
 do
 	if [[ -n $line ]];
 	then
@@ -28,10 +28,16 @@ do
 		echo $line
 	else
 		echo "sum is $sum"
+		if (($sum > $current_max_cals));
+		then
+			top_elf=$current_elf
+			current_max_cals=$sum
+		fi
 		sum=0
 		((current_elf++))
 		echo "current_elf is $current_elf"
+		
 	fi
 done < $file_in
 
-echo $sum
+echo "the top elf is at index $top_elf and is carrying $current_max_cals calories."
